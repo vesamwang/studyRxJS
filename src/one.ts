59b7e8f72f21c079525ea79113b72b1179acb892
szlevin <3259963@qq.com>
2016-09-28 16:45:30 +0800

    add

--- src/one.ts
+++ src/one.ts
@@ -13,3 +13,6 @@ Observable.of(1, 2, 3).map(x => console.log(x + '!!!')).subscribe();
 Observable.of(1, 2, 3).subscribe(v => console.log(v));
 
 Observable.of(1, 2, 3).scan((acc, curr) => acc + curr, 0).subscribe(v => console.log(v));
+
+
+
