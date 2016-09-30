2131a82ad709f119017610b32ea2b4f545a9000f
szlevin <3259963@qq.com>
2016-09-30 15:55:39 +0800

    add

--- src/observable.vs.subject.ts
+++ src/observable.vs.subject.ts
@@ -4,22 +4,44 @@
 
 import * as Rx from 'rxjs/Rx';
 
+/**
+ * observable 与 subject 的区别是
+ * - unicast vs. multicast (单路广播 vs. 多路广播)
+ *
+ * 举例: 发送 1 2 3 订阅者3个 s1 s2 s3
+ * - observable 的接收顺序:
+ *  1. s1 接收 1 2 3
+ *  2. s2 接收 1 2 3
+ *  3. s3 接收 1 2 3
+ * - subject 接收顺序
+ *  1. s1 s2 s3 接收 1
+ *  2. s1 s2 s3 接收 2
+ *  3. s1 s2 s3 接收 3
+ *
+ */
+
 const observable = Rx.Observable.create((observer: Rx.Observer<number>) => {
     observer.next(1);
     observer.next(2);
     observer.next(3);
 });
 
-observable.subscribe((x: number) => console.log('observable first subscribe', x));
-observable.subscribe((x: number) => console.log('observable second subscribe', x));
+observable.subscribe((x: number) => console.log('observable 1st subscribe', x));
+observable.subscribe((x: number) => console.log('observable 2nd subscribe', x));
+observable.subscribe((x: number) => setTimeout(() => console.log('observable async 1st subscribe', x), 0));
+observable.subscribe((x: number) => setTimeout(() => console.log('observable async 2nd subscribe', x), 1000));
+observable.subscribe((x: number) => setTimeout(() => console.log('observable async 3th subscribe', x), 1100));
 
 ///////////////////////////////////////////////////////
 console.log('='.repeat(80));
 
 const subject = new Rx.Subject();
 
-subject.subscribe((x: number) => console.log('subject first subscribe', x));
-subject.subscribe((x: number) => console.log('subject second subscribe', x));
+subject.subscribe((x: number) => console.log('subject 1st subscribe', x));
+subject.subscribe((x: number) => console.log('subject 2nd subscribe', x));
+subject.subscribe((x: number) => setTimeout(() => console.log('subject async 1st subscribe', x), 0));
+subject.subscribe((x: number) => setTimeout(() => console.log('subject async 2nd subscribe', x), 1000));
+subject.subscribe((x: number) => setTimeout(() => console.log('subject async 3th subscribe', x), 1100));
 
 subject.next(1);
 subject.next(2);
--- src/reference-counting.ts
+++ src/reference-counting.ts
@@ -28,7 +28,83 @@
 import * as Rx from 'rxjs/Rx';
 
 // 创建`observable` 每500ms发送一个递进数字 1, 2, 3,  ...
-const source = Rx.Observable.interval(500);
-const subject = new Rx.Subject() as Rx.Subject<number>;
+const source = Rx.Observable.interval(500); // 1)
+const subject = new Rx.Subject() as Rx.Subject<number>; // 2)
 // 创建'多路广播'
-const multicasted = source.multicast(subject);
+const multicasted = source.multicast(subject); // 3)
+
+const subscription1 = multicasted.subscribe({ // 4)
+    next: (v) => console.log('observerA:', v)
+});
+const subscriptionConnect = multicasted.connect(); //5)
+
+let subscription2: Rx.Subscription;
+setTimeout(() => { // 6)
+    subscription2 = multicasted.subscribe({
+        next: (v) => console.log('observerB:', v)
+    });
+}, 600);
+
+setTimeout(() => { // 7)
+    subscription1.unsubscribe();
+}, 1200);
+
+setTimeout(() => { //8)
+    subscription2.unsubscribe();
+    subscriptionConnect.unsubscribe();
+}, 2000);
+
+/**
+ * 上面的代码做了下面的事情:
+ * 1) 创建'可观察流' source
+ * 2) 创建'主题' subject
+ * 3) 将'可观察'流 source 与 '主题' subject 结合生成'多路广播-可观察流'
+ * 4) 创建第一个'订阅者' subscription1
+ * 5) 连接'多路广播-可观察流'使其开始发布数据
+ * 6) 600ms后创建第二个'订阅者' subscription2
+ * 7) 1200ms后第一个'订阅者' subscription1 取消订阅
+ * 8) 2000ms后第一个'订阅者' subscription1 取消订阅, 并且使'多路广播-可观察流'停止发布
+ *
+ * 这么多动作是期望实现如下的逻辑:
+ * 1. 创建多路广播
+ * 2. 第一个订阅者订阅后立即开始发布数据
+ * 3. 最后一个订阅者取消订阅后立即停止发布数据
+ *
+ * RxJS提供了一种便利的方法去实现上面的逻辑
+ * refCount makes the multicasted Observable automatically start executing when the first subscriber arrives,
+ * and stop executing when the last subscriber leaves.
+ */
+
+setTimeout(() => { // 避免与上面的异步代码输出内容冲突
+    console.log('='.repeat(80));
+    const source = Rx.Observable.interval(500);
+    const subject = new Rx.Subject() as Rx.Subject<number>;
+    const refCounted = source.multicast(subject).refCount();
+
+    console.log('observerA subscribed');
+    const subscription1 = refCounted.subscribe({
+        next: (v) => console.log('observeA:', v)
+    });
+
+    let subscription2: Rx.Subscription;
+    setTimeout(() => {
+        console.log('observerB subscribed');
+        subscription2 = refCounted.subscribe({
+            next: (v) => console.log('observeB:', v)
+        });
+    }, 600);
+
+    setTimeout(() => {
+        console.log('observerA unsubscribed');
+        subscription1.unsubscribe();
+    }, 1200);
+
+    setTimeout(() => {
+        console.log('observerB unsubscribed');
+        subscription2.unsubscribe();
+    }, 2000);
+
+}, 2500);
+
+
+
