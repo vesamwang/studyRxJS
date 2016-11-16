17a4c74712ea876f3a62e9f2284d19e90f2b7c07
szlevin <3259963@qq.com>
2016-11-16 14:12:22 +0800

    理解取消订阅

---
+++ src/multiSubject.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by 720718 on 2016/11/16.
+ */
+
+import * as Rx from 'rxjs';
+
+const sj = new Rx.Subject();
+
+const subscription1 = sj.subscribe((data) => console.log('first:', data));
+
+sj.next(1);
+// subscription1.unsubscribe();
+
+sj.subscribe((data) => console.log('second:', data));
+
+sj.next(2);
+
+sj.next(3);
